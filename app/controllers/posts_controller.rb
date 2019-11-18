40d089f05a611d687ff39a2695ce56517428c23b
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-18 13:32:17 +0900

    SendGrid Test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,9 @@ class PostsController < ApplicationController
   end
 
   def mail
-    TestMailer.testmail(params[:str]).deliver_later  #メーラに作成したメソッドを呼び出す。
-    flash[:notice] = "メール送信完了" 
+    NewsMailMailer.testmail(params[:str]).deliver_later  #メーラに作成したメソッドを呼び出す。
+    binding.pry
+    flash[:notice] = "メール送信完了"
     redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Rails.application.routes.draw
+Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, only: [:index, :show]
-    get '/sub.html', to: 'posts#show'
-    get 'about', to: 'posts#about'
-    post '/mail', to: 'posts#mail', as: :mail
+    get '/sub.html', to: 'posts#show';
+    get 'about', to: 'posts#about';
+    post '/mail', to: 'posts#mail', as: :mail;
 end
